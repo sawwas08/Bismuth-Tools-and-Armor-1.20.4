598b2571a48867f9d03b049036232ecbbc2b7602
sawwas08 <sawwas08@icloud.com>
2024-02-10 22:51:54 -0500

    initial mod upload

---
+++ src/main/java/net/sawwas08/bismuthmod/BismuthModClient.java
@@ -0,0 +1,10 @@
+package net.sawwas08.bismuthmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BismuthModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sawwas08/bismuthmod/BismuthModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sawwas08.bismuthmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BismuthModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
